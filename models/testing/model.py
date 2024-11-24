12ddad4b95a2b5caa7bfb887b86baa6b4e92d13b
ethanschmitt <ethanschmitt7@gmail.com>
2024-11-23 17:20:09 -0800

    Fix batch norm

--- models/testing/model.py
+++ models/testing/model.py
@@ -11,20 +11,20 @@ class Model(nn.Module):
         self.n = len(self.group_sizes)
 
         # Convolutional layers
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1, bias=False)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, bias=False)
+
+        # Batch normalization
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
 
         # Max pooling
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Dropout
         self.dropout = nn.Dropout(p=0.5)
-
-        # Batch normalization
-        self.bn1 = nn.BatchNorm2d(32)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.bn3 = nn.BatchNorm2d(128)
 
         # Fully connected layers
         self.flatten = nn.Flatten()
